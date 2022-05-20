0dd11a2fc7a411ae18ac439f36fd8a2cba44a382
vanesaSMP <105896232+vanesaSMP@users.noreply.github.com>
2022-05-20 12:18:49 +0700

    first upload

---
+++ database.js
@@ -0,0 +1,119 @@
+let makanan = [];
+
+let makanan_detail = {
+    Nama : "Jus",
+    Harga : 15000,
+    Kode : "BRG001",
+    Stock : 10,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/fsmqHnpYZHbXBQo46"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Bakso",
+    Harga : 15000,
+    Kode : "BRG002",
+    Stock : 20,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/hHFF11LMjpwZP2SV6"
+};
+makanan.push(makanan_detail)
+
+
+ makanan_detail = {
+    Nama : "Heci",
+    Harga : 2000,
+    Kode : "BRG003",
+    Stock : 100,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/KsYR1Bd2FvsWqmq89"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Permen",
+    Harga : 500,
+    Kode : "BRG004",
+    Stock : 1000,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/rEeKthunXrbSj5jz8"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Air mineral",
+    Harga : 6000,
+    Kode : "BRG005",
+    Stock : 50,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/3J9wy5JZpBr4bfZt8"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Croffle",
+    Harga : 5500,
+    Kode : "BRG006",
+    Stock : 20,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/3GCvbRwoBnNaYjmg9"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Cappucino",
+    Harga : 6000,
+    Kode : "BRG007",
+    Stock : 30,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/EmXYqMDuD1zBYekDA"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Pancake",
+    Harga : 5000,
+    Kode : "BRG008",
+    Stock : 30,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/DyMVE5vrzD99vgaE9"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Bread toast",
+    Harga : 4500,
+    Kode : "BRG009",
+    Stock : 30,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/qKMBw9kYQKuLpkXu8"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Boba",
+    Harga : 5500,
+    Kode : "BRG010",
+    Stock : 20,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/ucXtcjhTFmjtCUkNA"
+};
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    Nama : "Cocktail",
+    Harga : 6000,
+    Kode : "BRG011",
+    Stock : 15,
+    Rating : "5,0",
+    Gambar : "https://images.app.goo.gl/v9wDi7kUsLqWbnWG6"
+};
+
+makanan.push(makanan_detail)
+
+
+console.log(makanan);
+
+
+
